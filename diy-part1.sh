de3ddb91325ece4b2004957cdc944b1916ebd091
Doanduy09 <96837081+Doanduy09@users.noreply.github.com>
2022-01-19 21:26:30 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,4 @@
 # Add a feed source
 echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 
